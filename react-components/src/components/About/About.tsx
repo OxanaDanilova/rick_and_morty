0cb55818094c96969928013128d3df2628953995
Oxana Danilova <oxanadanilova11@gmail.com>
2022-09-27 15:19:12 +0200

    feat: create Home, About, NotFound pages

---
+++ react-components/src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>About us</h1>
+        </header>
+      </>
+    );
+  }
+}
---
+++ react-components/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h1>404 Page not found</h1>
+        </header>
+      </>
+    );
+  }
+}
