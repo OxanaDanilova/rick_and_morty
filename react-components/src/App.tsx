f967be83970ddd3a508bb2632c1ef60f4d9f9f84
Oxana Danilova <oxanadanilova11@gmail.com>
2022-10-02 10:33:19 +0200

    fix:change route for 404 page

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
