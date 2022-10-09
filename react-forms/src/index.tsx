130c3fe213c0c0c3ec2536fbe9b530771312e243
Oxana Danilova <oxanadanilova11@gmail.com>
2022-10-09 20:22:57 +0200

    feat: prepair to deploy

--- react-forms/src/index.tsx
+++ react-forms/src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router basename="/oxanadanilova-REACT2022Q3/react-components">
+    <Router basename="/oxanadanilova-REACT2022Q3/react-forms">
       <App />
     </Router>
   </React.StrictMode>
