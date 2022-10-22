72f40b7ba4cec4439119457881fda9313c06fb41
Oxana Danilova <oxanadanilova11@gmail.com>
2022-10-22 21:13:59 +0200

    feat: prepair to deploy

--- react-hooks/src/index.tsx
+++ react-hooks/src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router basename="/oxanadanilova-REACT2022Q3/react-api">
+    <Router basename="/oxanadanilova-REACT2022Q3/react-hooks">
       <App />
     </Router>
   </React.StrictMode>
